d9426cf1f33fb7f68670f1c622c2e029b2b08eb1
web3sandyyy <sandeeprajapati18@gmail.com>
2025-09-05 01:38:21 +0530

    prettier and eslint added

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist', 'build', 'coverage', 'node_modules'],
+  },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      // Prettier will handle formatting, so disable conflicting ESLint rules
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
   },
-])
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import Landing from './pages/Landing'
-import Explore from './pages/Explore'
-import ArtistProfile from './pages/ArtistProfile'
-import ArtistEdit from './pages/ArtistEdit'
-import ArtistDashboard from './pages/ArtistDashboard'
-import UserDashboard from './pages/UserDashboard'
-import Auth from './pages/Auth'
-import Admin from './pages/Admin'
-import AdminDashboard from './pages/AdminDashboard'
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Landing from './pages/Landing';
+import Explore from './pages/Explore';
+import ArtistProfile from './pages/ArtistProfile';
+import ArtistEdit from './pages/ArtistEdit';
+import ArtistDashboard from './pages/ArtistDashboard';
+import UserDashboard from './pages/UserDashboard';
+import Auth from './pages/Auth';
+import Admin from './pages/Admin';
+import AdminDashboard from './pages/AdminDashboard';
 
 function App() {
   return (
     <div>
-              <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/artist/:username" element={<ArtistProfile />} />
-          <Route path="/artist/:username/edit" element={<ArtistEdit />} />
-          <Route path="/artist/:username/dashboard" element={<ArtistDashboard />} />
-          <Route path="/user/dashboard" element={<UserDashboard />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/explore' element={<Explore />} />
+        <Route path='/artist/:username' element={<ArtistProfile />} />
+        <Route path='/artist/:username/edit' element={<ArtistEdit />} />
+        <Route
+          path='/artist/:username/dashboard'
+          element={<ArtistDashboard />}
+        />
+        <Route path='/user/dashboard' element={<UserDashboard />} />
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/admin/dashboard' element={<AdminDashboard />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
